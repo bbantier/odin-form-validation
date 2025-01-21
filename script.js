5488d9b086a500934929fb325a6030efc8aaad5e
bbantier <65014410+bbantier@users.noreply.github.com>
2025-01-21 15:07:03 +0100

    script.js target error spans

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ const zip = document.getElementById("zip-input");
 const pass = document.getElementById("pass-input");
 const passConf = document.getElementById("pass-conf-input");
 
+const emailError = document.querySelector(".email-error");
+const countryError = document.querySelector(".country-error");
+const zipError = document.querySelector(".zip-error");
+const passError = document.querySelector(".pass-error");
+const passConfError = document.querySelector(".pass-conf-error");
