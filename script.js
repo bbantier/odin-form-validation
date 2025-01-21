7de1e5af6747bf9a1134a84dbc2d65a4bb522737
bbantier <65014410+bbantier@users.noreply.github.com>
2025-01-21 15:04:38 +0100

    script.js target form elements

---
+++ script.js
@@ -0,0 +1,7 @@
+const form = document.querySelector(".test-form");
+const email = document.getElementById("email-input");
+const country = document.getElementById("country-input");
+const zip = document.getElementById("zip-input");
+const pass = document.getElementById("pass-input");
+const passConf = document.getElementById("pass-conf-input");
+
